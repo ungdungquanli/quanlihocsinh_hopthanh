b96f8b9f37139f89ccef19a200ee1837441f41f9
phanmemquanli <tulieuhopthanh@gmail.com>
2026-03-26 16:56:15 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -1,20 +1,20 @@
-// --- CẤU HÌNH HỆ THỐNG ---
-const linkapi = "https://script.google.com/macros/s/AKfycbwY_LDKefb80LVnegm_IwoSWzvpAuZB7cLqefApUW4eDiYG3Qc4JYqjLgoJT8SRKG9W/exec"; // Thay Link Web App (doPost) vào đây
+// --- CẤU HÌNH HỆ THỐNG GIAO TIẾP ---
+const hks_duongDanGiaoTiep_str = "https://script.google.com/macros/s/AKfycbwY_LDKefb80LVnegm_IwoSWzvpAuZB7cLqefApUW4eDiYG3Qc4JYqjLgoJT8SRKG9W/exec"; 
 
 // --- HÀM KẾT NỐI API CHUNG ---
-async function callServer(actionName, payload = {}) {
-    payload.action = actionName;
+async function hks_thucThiGiaoTiep_fn(hks_lenhDieuPhoi_str, hks_goiDuLieu_obj = {}) {
+    hks_goiDuLieu_obj.hks_lenhKichHoat_str = hks_lenhDieuPhoi_str;
     try {
-        const res = await fetch(linkapi, {
+        const hks_phanHoiNhanCung_obj = await fetch(hks_duongDanGiaoTiep_str, {
             method: 'POST',
             headers: {
                 "Content-Type": "text/plain;charset=utf-8",
             },
-            body: JSON.stringify(payload)
+            body: JSON.stringify(hks_goiDuLieu_obj)
         });
-        return await res.json();
-    } catch (error) {
-        console.error("Lỗi Fetch API:", error);
+        return await hks_phanHoiNhanCung_obj.json();
+    } catch (hks_loiNgoaiLe_obj) {
+        console.error("Lỗi Fetch API:", hks_loiNgoaiLe_obj);
         throw new Error("Lỗi kết nối Server. Vui lòng kiểm tra lại mạng hoặc link API!");
     }
 }
